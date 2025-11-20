ae8791c604df49577fb765fd56e31869ab817f76
bebejan01 <mustafademirel047@gmail.com>
2025-11-20 22:33:22 +0300

    Added

---
+++ Hafta_05/script_bir.js
@@ -0,0 +1,60 @@
+// JavaScript source code
+
+console.log('JS basladi')
+
+console.log('1234')
+console.log(true)
+console.log([1, 2, 3])
+console.error('Hata mesaj1')
+console.warn('uyari mesaji')
+
+// Değişkenler
+
+var age;
+console.log(age);
+
+var fullname = "brkcbk"
+console.log(fullname);
+
+// değiken isimleri sayi ile başlayamaz,
+// boşluk içeremez
+
+let isim = "brk"
+console.log(isim);
+
+// Büyük küçük harf duyarlılığı vardır
+
+var fullname = "brkcbk"
+
+var Fullname = "brbk"
+console.log(fullname)
+console.log(Fullname)
+
+
+let firstname = 'burak'
+console.log(typeof firstname)
+
+let yasim = 20
+console.log(typeof yasim) // number
+
+let money = 100.5
+console.log(typeof money) // number
+
+let isActive = false
+console.log(typeof isActive) // boolean
+
+let car;
+console.log(typeof car) // undefined
+
+let names = ['Ali', 'Ayşe', 'Burak']
+console.log(typeof names) // array
+let adres = {
+    city: 'Bilecik',
+    country: 'Türkiye'
+
+}
+console.log(typeof adres) // object
+
+console.log(names)
+console.log(adres)
+
